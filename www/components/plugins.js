5b64f72295ca4348d6c6132c4b7d22ed87959f8c
fabiodtna <sansdtna@gmail.com>
2020-05-19 15:53:50 +0900

    3commit

--- www/components/plugins.js
+++ www/components/plugins.js
@@ -1,4 +1,5 @@
 
+ //button alert js com confirmaçao
 $(document).on("click","#teste",function(){
     function retorno(){
        // alguma funçao a mais no buton alert para executa!
@@ -6,9 +7,27 @@ $(document).on("click","#teste",function(){
     navigator.notification.alert("YOU DON'T BELIVER!",retorno,"Aviso!","Aceito");
 });
 
+// usando o alert para escolher uma opçaõ
 $(document).on("click","#b2",function(){
   function confirma(buttonIndex){
-    navigator.notification.alert(buttonIndex);
+   if(buttonIndex == 1){
+     navigator.notification.alert("Escolheu a opção A");
+   }else{
+     navigator.notification.alert("Escolheu a opção B");
+   }
   }
+  //['A','B'] opções para escolher! 
   navigator.notification.confirm("Escolha A ou B!",confirma,"Escolha",['A','B']);
+});
+
+// beep no modo notificação
+
+$(document).on("click","#b3",function(){
+    navigator.notification.beep(5);
+    //(5) cinco beep
+});
+
+// vibração
+$(document).on("click","#b4",function(){
+    navigator.vibrate(2000);
 });
