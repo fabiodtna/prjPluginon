53ac570f109fd7b3a892158a43c3de3ccaa8ddbf
fabiodtna <sansdtna@gmail.com>
2020-05-19 15:26:14 +0900

    2 commit

--- www/components/plugins.js
+++ www/components/plugins.js
@@ -1,4 +1,14 @@
 
-$(document).on("click"."#alert",function(){
-    navigator.notification.alert("YOU DON'T BELIVER!");
+$(document).on("click","#teste",function(){
+    function retorno(){
+       // alguma funçao a mais no buton alert para executa!
+    }
+    navigator.notification.alert("YOU DON'T BELIVER!",retorno,"Aviso!","Aceito");
+});
+
+$(document).on("click","#b2",function(){
+  function confirma(buttonIndex){
+    navigator.notification.alert(buttonIndex);
+  }
+  navigator.notification.confirm("Escolha A ou B!",confirma,"Escolha",['A','B']);
 });
